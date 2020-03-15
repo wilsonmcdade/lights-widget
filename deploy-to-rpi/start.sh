5658c2dd8e04ebc81289ec63bad694b18b16b27c
Wilson McDade <wilson.mcdade@gmail.com>
2020-03-15 02:28:06 -0400

    Updated start.sh

--- deploy-to-rpi/start.sh
+++ deploy-to-rpi/start.sh
@@ -1,1 +1,3 @@
-sudo PYTHONPATH=".:build/lib.linux-armv7l-2.7" python app.py
+cd /home/pi/lights-widget/deploy-to-rpi
+. venv/bin/activate
+PYTHONPATH=".:build/lib.linux-armv7l-2.7" flask run --host=0.0.0.0 --port=8080
